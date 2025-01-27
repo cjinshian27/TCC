5c337d54624a2476ace3ba034618f6137c1d9deb
Jin Shian <cjinshian77@usp.br>
2025-01-27 19:44:16 -0300

    feat(splay_tree/):
    	Adding a new folder that contains everything related to splay
    	trees.

---
+++ splay_tree/node.hpp
@@ -0,0 +1,23 @@
+template <typename Key>
+
+class Node{
+	
+	public:
+		Node * parent;
+		Node * right;
+		Node * left;
+		Key key;
+
+		//key setter
+		void setKey(Key key){
+			this->key = key;
+		}
+
+		//node's parameterized constructor
+		Node(Key key){
+			this->key = key;
+			parent = nullptr;
+			right = nullptr;
+			left = nullptr;
+		}	
+};
