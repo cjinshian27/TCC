0c9d7bf4f7358b8dea2365296e7bcd245eda9ce8
Jin Shian <cjinshian77@usp.br>
2025-03-04 09:06:09 -0300

    refactor(main.cpp):
    	Refactoring the main.cpp code by adding some
    	whitespaces to make the code more readable.

--- dynamic_forest/main.cpp
+++ dynamic_forest/main.cpp
@@ -15,10 +15,13 @@ void decode(){
 	DynamicForest<int> * dynamicForest = new DynamicForest<int>();
 
 	while(std::cin >> operation){
+		
 		switch(operation){
+			
 			case 1:
 				std::cin >> n;
 				break;
+
 			case 2:
 				std::cin >> u >> v;
 				if(dynamicForest->isConnected(u, v)){
@@ -28,14 +31,17 @@ void decode(){
 					std::cout << "False" << '\n';
 				}
 				break;
+			
 			case 3:
 				std::cin >> u >> v;
 				dynamicForest->link(u, v);
 				break;
+			
 			case 4:
 				std::cin >> u >> v;
 				dynamicForest->cut(u, v);
 				break;
+			
 			case 5:
 				dynamicForest->print();
 				break;
@@ -44,7 +50,6 @@ void decode(){
 
 }
 
-//main function
 int main(){
 	decode();
 }
