4e6e6b55585e07f75e0aac0f10a29801e23b052b
Jin Shian <cjinshian77@usp.br>
2025-03-16 11:21:45 -0300

    feat(dynamicGraph):
    	Adding body to the constructor and the add()
    	method. It is uncompleted, and perhaps it
    	might not compile.

--- dynamic_graph/dynamicGraph.hpp
+++ dynamic_graph/dynamicGraph.hpp
@@ -1,16 +1,35 @@
+#include <iostream>
+#include <cmath>
+
 template<typename Key>
 
 class DynamicGraph{
 
 	private:
 
+		std::vector<adjacencyList<Key> *> * adjacencyLists;
+		std::vector<Forest<Key> *> * forests;
+
+		unsigned int n;
+	
 	public:
 
-		DynamicGraph(){
-
+		DynamicGraph(unsigned int n){
+			adjacencyLists = new std::vector<adjacencyList<Key> *>(n + 1);
+			forests = new std::vector<Forest<Key> *>(n + 1);
 		}
 
-		void connect(Key u, Key v){
+		void add(Forest<Key> * forest, Key u, Key v){
+			unsigned int level = static_cast<int>(std::ceil(std::log2(n))); 
+
+			Edge<Key, Key> * edge = new Edge<Key, Key>(u, v);
+
+			if(forest->isConnected(u, v)){
+				this->adjacencyLists<Key>[level]->insert(u, v);
+			}
+			else{
+
+			}
 
 		}
 
@@ -19,6 +38,6 @@ class DynamicGraph{
 		}
 
 		bool isConnected(Key u, Key v){
-
+			return true;
 		}
 }
