63287cea51dd39b11f033609d16ad4eab5c07b23
Jin Shian <cjinshian77@usp.br>
2025-03-16 07:32:17 -0300

    feat(dynamicGraph):
    	Adding the main methods for the class. They
    	still do not have a body.

--- dynamic_graph/dynamicGraph.hpp
+++ dynamic_graph/dynamicGraph.hpp
@@ -4,12 +4,21 @@ class DynamicGraph{
 
 	private:
 
-
-
-
-
-
-
 	public:
 
+		DynamicGraph(){
+
+		}
+
+		void connect(Key u, Key v){
+
+		}
+
+		void remove(Key u, Key v){
+
+		}
+
+		bool isConnected(Key u, Key v){
+
+		}
 }
