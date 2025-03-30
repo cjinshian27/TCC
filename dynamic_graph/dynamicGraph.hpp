828f79913a8b0a73d0e75b4308ffc9e631eb946b
Jin Shian <cjinshian77@usp.br>
2025-03-30 17:18:30 -0300

    feat(dynamicGraph.hpp):
    	Initializing the 'adjacencyLists' and 'forests'
    	vectors in the constructor.

--- dynamic_graph/dynamicGraph.hpp
+++ dynamic_graph/dynamicGraph.hpp
@@ -7,7 +7,8 @@ class DynamicGraph{
 
 	private:
 
-		std::vector<adjacencyList<Key> *> * adjacencyLists;
+		std::vector<AdjacencyList<Key> *> * adjacencyLists;
+		
 		std::vector<Forest<Key> *> * forests;
 
 		unsigned int n;
@@ -15,20 +16,29 @@ class DynamicGraph{
 	public:
 
 		DynamicGraph(unsigned int n){
-			adjacencyLists = new std::vector<adjacencyList<Key> *>(n + 1);
+			
+			adjacencyLists = new std::vector<AdjacencyList<Key> *>(n + 1);
 			forests = new std::vector<Forest<Key> *>(n + 1);
+			
+			for(int i = 0; i < level + 1; ++i){
+				adjacencyLists[i] = new AdjacencyList<Key>();
+				forests[i] = new Forest<Key>();
+			}
 		}
 
 		void add(Forest<Key> * forest, Key u, Key v){
 			unsigned int level = static_cast<int>(std::ceil(std::log2(n))); 
 
 			Edge<Key, Key> * edge = new Edge<Key, Key>(u, v);
+			
+			AdjacencyList<Key> * adjList = this->adjacencyLists[level];
+			Forest<Key> * forest = this->forests[level];
 
 			if(forest->isConnected(u, v)){
-				this->adjacencyLists<Key>[level]->insert(u, v);
+				adjList->insert(u, v);
 			}
 			else{
-
+				
 			}
 
 		}
