ed00cd1767455ab464badcb648c1e6ffff0f7394
Jin Shian <cjinshian77@usp.br>
2025-04-21 10:41:06 -0300

    feat: add methods that previously existed in the dynamic_forest edge.hpp folder.

--- dynamic_graph/edge.hpp
+++ dynamic_graph/edge.hpp
@@ -8,6 +8,45 @@ class Edge{
 		Key v;
 
 	public:
+	
+		unsigned int id;
+		std::pair<Key, Key> edge;
+		unsigned int size;
+		
+		Edge * left = nullptr;
+		Edge * right = nullptr;
+		Edge * parent = nullptr;
+
+		Edge(Key first, Key second, unsigned int id){
+
+			this->edge.first = first;
+			this->edge.second = second;
+			this->id = id;
+			this->size = 1;
+		}
+
+		//print the edge in the (u, v) format
+		void print(){
+
+			std::cout << "(" << this->edge.first << " " << this->edge.second << ")" << '\n';
+		}
+
+		//set the edge size
+		void setSize(){
+
+			if(this->left && this->right){
+				this->size = 1 + this->left->size + this->right->size;
+			}
+			else if(this->left){
+				this->size = 1 + this->left->size;
+			}
+			else if(this->right){
+				this->size = 1 + this->right->size;
+			}
+			else{
+				this->size = 1;
+			}
+		}
 
 		void setLevel(unsigned int level){
 			
