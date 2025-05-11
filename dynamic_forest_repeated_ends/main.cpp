0fbc1e9b42fedcc2be349a83c88b4cc8caad46a6
Jin Shian <cjinshian77@usp.br>
2025-05-11 11:09:07 -0300

    chore: adapt the function to the dynamic forest api        changes.

--- dynamic_forest_repeated_ends/main.cpp
+++ dynamic_forest_repeated_ends/main.cpp
@@ -10,40 +10,43 @@
 //decode function
 void decode(){
 
-	int operation, n, u, v;
+	int operation, u, v;
+	unsigned int n;
 
-	DynamicForest<int> * dynamicForest;
+	std::cin >> n;
+	std::vector<int> vertices(n);
+
+	for(unsigned int i = 1; i <= n; ++i){
+		vertices[i-1] = i;
+	}
+
+	DynamicForest<int> * dynamicForest = new DynamicForest<int>(vertices);
 
 	while(std::cin >> operation){
 		
 		switch(operation){
 			
 			case 1:
-				std::cin >> n;
-				dynamicForest = new DynamicForest<int>(n);
-				break;
-
-			case 2:
 				std::cin >> u >> v;
 				if(dynamicForest->isConnected(u, v)) std::cout << "True" << '\n';
 				else std::cout << "False" << '\n';
 				break;
 			
-			case 3:
+			case 2:
 				std::cin >> u >> v;
 				dynamicForest->link(u, v);
 				break;
 			
-			case 4:
+			case 3:
 				std::cin >> u >> v;
 				dynamicForest->cut(u, v);
 				break;
 			
-			case 5:
+			case 4:
 				dynamicForest->print();
 				break;
 
-			case 6:
+			case 5:
 				dynamicForest->printTrees();
 				break;
 		}
