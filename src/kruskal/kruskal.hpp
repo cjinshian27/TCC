a6d2df5990cb91918d0e54c97796c468cfbb36ad
Jin Shian <cjinshian77@usp.br>
2025-12-13 14:55:12 -0300

    chore: make kruskal more efficient

--- src/kruskal/kruskal.hpp
+++ src/kruskal/kruskal.hpp
@@ -15,8 +15,6 @@ private:
     std::vector<Edge> mst_edges;
     UnionFind* uf;
     long long mst_weight;
-    bool mst_built;
-    bool removed = false;
     
     // make u < v
     std::pair<int,int> normalize_edge(int u, int v) {
@@ -24,50 +22,6 @@ private:
         return {u, v};
     }
 
-public:
-    Graph(int vertices) : V(vertices), mst_weight(0), mst_built(false) {
-        uf = new UnionFind(V);
-    }
-    
-    ~Graph() {
-        delete uf;
-    }
-    
-    // and an weighted edge
-    void addEdge(int u, int v, int weight) {
-        edges.emplace_back(u, v, weight);
-        mst_built = false;  // MST needs to be rebuilt
-    }
-    
-    // remove an edge
-    void removeEdge(int u, int v) {
-        removed = true;
-        std::pair<int,int> target_edge = normalize_edge(u, v);
-        
-        // Find the edge in the vector
-        bool found = false;
-        for (int i = 0; i < edges.size(); i++) {
-            if (normalize_edge(edges[i].u, edges[i].v) == target_edge) {
-                // Move the found edge to the end
-                std::swap(edges[i], edges[edges.size() - 1]);
-                
-                // Remove it using pop_back()
-                edges.pop_back();
-                
-                found = true;
-                mst_built = false;  // MST needs to be rebuilt
-
-                break;
-            }
-        }
-        
-        if (!found) {
-            return;
-        }
-        
-        buildMST();
-    }
-    
     void insertionSort(std::vector<Edge>& edges) {
         int n = edges.size();
         for (int i = 1; i < n; i++) {
@@ -83,8 +37,8 @@ public:
         }
     }
 
-    // build MST using Kruskal's algorithm
-    void buildMST() {
+        // build MST using Kruskal's algorithm
+    void rebuildMST() {
         
         // reset Union-Find and MST data
         delete uf;
@@ -92,15 +46,8 @@ public:
         mst_edges.clear();
         mst_weight = 0;
         
-        // sort edges by weight
-        if(!removed){
-            // O(n lg n) sorting 
-            sort(edges.begin(), edges.end());
-        }
-        else{
-            // linear sorting for nearly sorted arrays
-            insertionSort(edges);
-        }
+        // sort in O(n) time in a nearly sorted array
+        insertionSort(edges);
                 
         // process edges in order
         for (const auto& edge : edges) {
@@ -113,25 +60,75 @@ public:
             }
         }
         
-        mst_built = true;
+    }
+
+
+public:
+    Graph(int vertices) : V(vertices), mst_weight(0){
+        uf = new UnionFind(V);
+    }
+    
+    ~Graph() {
+        delete uf;
+    }
+    
+    // and an weighted edge
+    void addEdge(int u, int v, int weight) {
+        edges.emplace_back(u, v, weight);
+    }
+    
+    // remove an edge
+    void removeEdge(int u, int v) {
+        std::pair<int,int> target_edge = normalize_edge(u, v);
+        
+        // Find the edge in the vector
+        bool found = false;
+        for (int i = 0; i < edges.size(); i++) {
+            if (normalize_edge(edges[i].u, edges[i].v) == target_edge) {
+                std::swap(edges[i], edges[edges.size() - 1]);
+                
+                edges.pop_back();
+                
+                found = true;
+
+                break;
+            }
+        }
+        
+        if (!found) {
+            return;
+        }
+        rebuildMST();
+    }
+
+    // build MST using Kruskal's algorithm
+    void buildMST() {
+        
+        // sort edges by increasing weight
+        sort(edges.begin(), edges.end());
+                
+        // process edges in order
+        for (const auto& edge : edges) {
+            if (uf->unite(edge.u, edge.v)) {
+                mst_edges.push_back(edge);
+                mst_weight += edge.weight;
+                
+                // MST complete when we have V-1 edges
+                if (mst_edges.size() == V - 1) break;
+            }
+        }
     }
     
     // check connectivity between two vertices
-    bool areConnected(int u, int v) {
-        if (!mst_built) {
-            buildMST();
-        }
-        
+    bool areConnected(int u, int v) {        
         if (u < 0 || u >= V || v < 0 || v >= V) {
             return false;
         }
-        
         return uf->connected(u, v);
     }
     
     // get number of components
     int getNumComponents() {
-        if (!mst_built) buildMST();
         return uf->getNumComponents();
     }
     
@@ -142,8 +139,6 @@ public:
 
     // print MST weighted edges
     void printWeightedEdges() {
-        if (!mst_built) buildMST();
-
         for (const auto& edge : mst_edges) {
             std::cout << "(" << edge.u << ", " << edge.v << ", weight: " << edge.weight  << ")\n";
         }
